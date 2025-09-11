9b599f7a7eab8267f0609b5c52357dfa6aa65dbd
CQuel Demo <demo@cquel.com>
2025-09-11 18:41:35 +0100

    Update branding to Sustainable Adventure and change user to Andy
    - Changed company name from 'Your Company' to 'Sustainable Adventure'
    - Updated logo to use 'sustainable advantage.png'
    - Changed all welcome messages from 'Welcome back Gemma' to 'Welcome back Andy'
    - Updated avatar name from 'Gemma' to 'Andy'
    - Applied changes across all dashboard tabs (Focus, Tenders, Surveys, Pricing, Contracts, Briefs)

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -43,7 +43,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={customerLogoSrc || "/logos/Company logos/bcra-new.png"}
+              src={customerLogoSrc || "/logos/Company logos/sustainable advantage.png"}
               alt={`${customerName} logo`}
               className="h-[48px] w-auto object-contain"
               onError={(e) => {
@@ -105,7 +105,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
             </div>
           )}
           
-          <Avatar name="Gemma" size={36} />
+          <Avatar name="Andy" size={36} />
         </div>
       </div>
     </div>
