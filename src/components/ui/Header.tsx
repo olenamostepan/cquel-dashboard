eed0cbbbd91f75c31da3f229a1b9e543e1631e5b
CQuel Demo <demo@cquel.com>
2025-09-25 12:18:48 +0100

    Update logo to M&P and change welcome message from Paul to Miguel

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -43,7 +43,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src={customerLogoSrc || "/logos/Company logos/roundhillcapital_logo_full.svg"}
+              src={customerLogoSrc || "/logos/Company logos/m&p.png"}
               alt={`${customerName} logo`}
               className="h-[48px] w-auto object-contain"
               onError={(e) => {
@@ -105,7 +105,7 @@ const CoBrandingHeader: React.FC<CoBrandingProps> = ({ customerName, customerLog
             </div>
           )}
           
-          <Avatar name="Paul" size={36} />
+          <Avatar name="Miguel" size={36} />
         </div>
       </div>
     </div>
